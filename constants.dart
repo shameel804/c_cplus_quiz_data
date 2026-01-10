50c949ddf086354b179e651dad7643f06dc7713d
shameel804 <shameeabid2@gmail.com>
2026-01-10 14:39:14 +0530

    edit

---
+++ constants.dart
@@ -0,0 +1,37 @@
+class Constants {
+  static const Map allTypes = {
+    "C": [
+      ['basics', 'Basics of C', 'free'],
+      ['ops', 'Operators and Expressions', 'free'],
+      ['control', 'Control Structures', 'free'],
+      ['func', 'Functions', 'free'],
+      ['array', 'Arrays and Strings', 'free'],
+      ['ptr', 'Pointers', 'free'],
+      ['struct', 'Structures and Unions', 'vip'],
+      ['file', 'File Handling', 'vip'],
+      ['mem', 'Dynamic Memory Allocation', 'vip'],
+    ],
+    "Cpp": [
+      ['cpp_basics', 'Basics of C++ and OOP', 'free'],
+      ['classes', 'Classes and Objects', 'free'],
+      ['inherit', 'Inheritance', 'free'],
+      ['poly', 'Polymorphism', 'free'],
+      ['encap', 'Encapsulation and Abstraction', 'free'],
+      ['templates', 'Templates and STL', 'free'],
+      ['exceptions', 'Exception Handling', 'vip'],
+      ['file_cpp', 'File Handling in C++', 'vip'],
+      ['adv_cpp', 'Advanced Topics in C++', 'vip'],
+    ],
+    "Csharp": [
+      ['cs_basics', 'Basics of C#', 'free'],
+      ['oop_cs', 'OOP Concepts in C#', 'free'],
+      ['inherit_cs', 'Inheritance and Polymorphism', 'free'],
+      ['delegate', 'Delegates and Events', 'free'],
+      ['collections', 'Collections and Generics', 'free'],
+      ['exceptions_cs', 'Exception Handling', 'free'],
+      ['file_cs', 'File and Data Handling', 'vip'],
+      ['threading', 'Multithreading', 'vip'],
+      ['ui', 'WPF', 'vip'],
+    ],
+  };
+}
